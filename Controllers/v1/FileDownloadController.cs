a871262018a5d332eafe906e7fe3b4c7824d626b
mikemalino <mike_malinowski@premierinc.com>
2021-10-06 16:18:08 -0400

    Update FileDownloadController.cs

--- Controllers/v1/FileDownloadController.cs
+++ Controllers/v1/FileDownloadController.cs
@@ -46,7 +46,15 @@ namespace Premier.API.FileUploadDownload.Controllers
 		{
 			try
 			{
-				var result = await _fileDownloadService.GetFiles(fsEntriesID);
+				var result = (dynamic)null;
+				if (fsEntriesID == 0)
+				{
+					result = await _fileDownloadService.GetFiles(fsEntriesID);
+				}
+				else
+				{
+					result = await _fileDownloadService.GetFile(fsEntriesID);
+				}
 				if (result != null)
 				{
 					return new ApiResponse(result);
@@ -62,72 +70,5 @@ namespace Premier.API.FileUploadDownload.Controllers
 			}
 		}
 
-		//[HttpPost("upload")]
-		//[ProducesResponseType(typeof(ApiResponse), Status201Created)]
-		//[ProducesResponseType(typeof(ApiResponse), Status422UnprocessableEntity)]
-		//[ProducesResponseType(typeof(ApiResponse), Status400BadRequest)]
-		//public async Task<ApiResponse> UploadFile([FromForm] FileUploadRequest createFileUploadRequest)
-		//{
-		//    try
-		//    {
-		//        var fileUpload = _mapper.Map<FileUpload>(createFileUploadRequest);
-		//        var returnval = await _fileUploadService.UploadAsync(fileUpload);
-
-		//        //return Ok(new { files.Count, size = FileService.SizeConverter(files.Sum(files => files.length))});
-		//        return await Task.FromResult(new ApiResponse("Record Successfully Created", null, Status201Created));
-		//    }
-		//    catch (Exception ex)
-		//    {
-		//        throw new ApiException($"File Upload Failed {ex.Message}",Status400BadRequest);
-
-		//    }
-		//}
-
-		//[HttpPost]
-		//[ValidateModel]
-		//[Authorize]
-		//[ProducesResponseType(typeof(NoteRecordResponse), Status200OK)]
-		//[Route("updatenotetext")]
-		//public async Task<ApiResponse> UpdateNoteText([FromBody] NoteUpdateRequest noteUpdateRequest)
-		//{
-		//    try
-		//    {
-		//        return new ApiResponse($"Note text updated.", await _notesAPIService.UpdateNoteText(noteUpdateRequest));
-		//    }
-		//    catch (Exception e)
-		//    {
-		//        throw new ApiException(e, Status422UnprocessableEntity);
-		//    }
-		//}
-
-		//[HttpDelete]
-		//[Authorize]
-		//[ProducesResponseType(typeof(int), Status200OK)]
-		//[Route("deletenotetext/{notesID}")]
-		//public async Task<ApiResponse> DeleteNoteText(int notesID)
-		//{
-		//    try
-		//    {
-		//        int result = await _notesAPIService.DeleteNoteText(notesID);
-
-		//        if (result > 0)
-		//            return new ApiResponse($"Note text ID ({notesID}) deleted.", result, 200);
-		//        else
-		//            return new ApiResponse($"Note ID ({notesID}) not found.", result, 200);
-		//    }
-		//    catch (Exception e)
-		//    {
-		//        throw new ApiException(e, Status422UnprocessableEntity);
-		//    }
-		//}
-
-		//[HttpGet]
-		//[Authorize]
-		//[ProducesResponseType(typeof(List<NoteRecordResponse>), Status200OK)]
-		//[Route("GetByEntityID/{entityID}/type/{entityType}")]
-		//public async Task<ApiResponse> GetNotesByEntityID(int entityID, int entityType)
-		//{
-		//    return new ApiResponse(await _notesAPIService.GetNotesByEntityID(entityID, entityType));
-		//}
 	}
 }
